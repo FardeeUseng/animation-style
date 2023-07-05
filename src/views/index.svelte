88207a539b3c9291de286ba331427a1274df2a37
Dee <fardee.tat@gmail.com>
2023-07-05 15:31:31 +0700

    -feat:circle item

--- src/views/index.svelte
+++ src/views/index.svelte
@@ -1,10 +1,13 @@
 <script>
+  import { navigate } from "svelte-routing";
   let params = new URLSearchParams(window.location.search);
   let g = params.get("g");
   let v = params.get("v");
 
   const handleChangeParams = (group, various) => {
-    window.location.replace(`?g=${group}&v=${various || ""}`);
+    if (group) g = group;
+    if (various) v = various;
+    navigate(`/?g=${g}&v=${v}`, { replace: true });
   };
 
   let circleBox;
@@ -35,21 +38,31 @@
   on:mousemove={handleMouseMove}
 >
   {#each ["scale-up", "scale-down", "rotate", "rotate-scale", "rotate-90", "flip", "flip-2", "flip-scale", "flip-scale-2", "swing", "slide", "slide-bck", "slide-fwd", "slide-rotate", "shadow-drop", "shadow-drop-2", "shadow-pop", "shadow-inset"] as group}
-    <button
-      style="background-color: {group === g ? '#c1876b' : ''};"
-      class="circle"
-      on:click={() => handleChangeParams(group)}
-    >
-      <h4>{group}</h4>
-    </button>
-    <div />
+    <div class="circle-group">
+      <button
+        style="background-color: {group === g ? '#c1876b' : ''};"
+        class="circle"
+        on:click={() => handleChangeParams(group)}
+      >
+        <h4>{group}</h4>
+      </button>
+      {#if group === g}
+        <div>
+          <i class="fa-solid fa-caret-up" />
+        </div>
+      {/if}
+    </div>
   {/each}
 </div>
 <div class="circle-box-item">
   {#each [...Array(12)] as _, i}
-    <div>{i + 1}</div>
+    <button
+      class={v === i + 1 ? "isVariousSelected" : ""}
+      on:click={() => handleChangeParams(_, i + 1)}>{i + 1}</button
+    >
   {/each}
 </div>
+<p>{g}</p>
 
 <style>
   .circle-box {
@@ -57,7 +70,7 @@
     column-gap: 10px;
     overflow-x: auto;
     white-space: nowrap;
-    padding: 10px;
+    padding: 10px 10px 0 10px;
     background-color: #fff;
     border-bottom: 1px solid #646b63;
   }
@@ -78,6 +91,19 @@
     text-transform: uppercase;
   }
 
+  .circle-group > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .circle-group > div > i {
+    padding: 0;
+    height: 10px;
+    color: #646b63;
+  }
+
   .circle-box::-webkit-scrollbar {
     width: 0;
     height: 0;
@@ -90,17 +116,28 @@
     background-color: #fff;
     padding: 10px 20px 10px 20px;
     column-gap: 10px;
-    row-gap: 10px;
+    row-gap: 3px;
   }
 
-  .circle-box-item > div {
+  .circle-box-item > button {
     display: flex;
     align-items: center;
     justify-content: center;
     height: 30px;
     border-radius: 5px;
+    border: none;
+    background-color: #fff;
+    cursor: pointer;
   }
-  .circle-box-item > div:hover {
+  .circle-box-item > button:hover {
     color: red;
+  }
+
+  .isVariousSelected {
+    background-color: #c1876b !important;
+    color: #fff;
+  }
+  .isVariousSelected:hover {
+    color: #fff !important;
   }
 </style>
